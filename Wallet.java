56190a97b7373a8bfaf0891a413344082ec5245b
SalmaSherif1 <119257520+SalmaSherif1@users.noreply.github.com>
2022-11-28 22:33:38 +0200

    adding wallet class

---
+++ Wallet.java
@@ -0,0 +1,13 @@
+public class Wallet {
+    float funds;
+
+    public void AddFunds(float f) {
+        this.funds += f;
+    }
+
+    public float GetFunds() {
+        return funds;
+
+    }
+
+}
