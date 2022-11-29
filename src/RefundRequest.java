060100864721b0b674d4fb2d165cf18ad1c455d6
SalmaSherif1 <119257520+SalmaSherif1@users.noreply.github.com>
2022-11-29 12:24:02 +0200

    "Creating RefundRequest Class"

---
+++ src/RefundRequest.java
@@ -0,0 +1,28 @@
+public class RefundRequest {
+    static int num_requests = 1;
+    int id = 0;
+    float Refund;
+    String service;
+
+    public RefundRequest() {
+        id = num_requests++;
+
+    }
+
+    public int GetID() {
+        return id;
+    }
+
+    public void askForRefund(float f, String s) {
+        this.Refund = f;
+        this.service = s;
+    }
+
+    public float getRefundAmount() {
+        return Refund;
+    }
+
+    public String getServiceName() {
+        return service;
+    }
+}
