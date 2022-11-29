ef605e14123ac0a87aeedb4c45e56568fbd53af6
Norhan-Hassan <96375919+Norhan-Hassan@users.noreply.github.com>
2022-11-29 15:00:04 +0200

    Add files via upload

---
+++ CreditCard.java
@@ -0,0 +1,42 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CreditCard implements Payment{
+    private long ccNumber;
+    private String cvv;
+    private String dateOfExpiry;
+
+    CreditCard(long ccNum, String cvv, String expiryDate)
+    {
+        this.ccNumber=ccNum;
+        this.cvv=cvv;
+        this.dateOfExpiry=expiryDate;
+    }
+    //function that check on Expiration date
+    public boolean checkExpire()
+    {
+        boolean expired;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/yy");
+        simpleDateFormat.setLenient(false);
+        Date expiry = null;
+        try
+        {
+            expiry = simpleDateFormat.parse(dateOfExpiry);
+        }
+        catch (ParseException e)
+        {
+            throw new RuntimeException(e);
+        }
+        expired = expiry.before(new Date());
+        return expired;
+    }
+
+    public void pay()
+    {
+            if(checkExpire()==false)
+            {
+                System.out.println("you can pay by credit card technique");
+            }
+    }
+}
