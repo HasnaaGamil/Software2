25be643d17e0ea9cae5ee5ea4342e0902bdd24a7
Norhan-Hassan <nwrhanhsnly@gmail.com>
2022-12-03 12:04:48 +0200

    Check on Discount

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
 	public static void main(String[] args) {
+		Payment payment = new Cash();
+		payment = new OverallDiscount(payment);
+		System.out.println(payment.getDiscreption());
+		payment.pay();
 
 	}
 
