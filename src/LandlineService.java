5f8c1719f6e4769136947088473634cb5981d5e9
SalmaSherif1 <119257520+SalmaSherif1@users.noreply.github.com>
2022-12-03 13:31:15 +0200

    "Creating LandlineService class"

---
+++ src/LandlineService.java
@@ -0,0 +1,9 @@
+public class LandlineService extends Service {
+    public void setFields() {
+        form.put("Area code ", "");
+        form.put("Landline number", "");
+        form.put("Amount", "");
+
+    }
+
+}
