aeb16922473cc231fb377b8860676fc5fcc359f9
Norhan-Hassan <nwrhanhsnly@gmail.com>
2022-12-05 13:10:38 +0200

    Add setter and getter to payment class

--- src/Regestration.java
+++ src/Regestration.java
@@ -3,33 +3,31 @@ import java.util.List;
 
 public class Regestration {
 
-	List<User>users= new ArrayList<User> ();
-	public boolean searchUser(User user)
-	{ 
-		for(int i=0 ;i<users.size();i++)
-		{
-			if(user.getUserName()==users.get(i).getUserName()||user.getEmail()==users.get(i).getEmail())
+	List<User> users = new ArrayList<User>();
+
+	public boolean searchUser(User user) {
+		for (int i = 0; i < users.size(); i++) {
+			if (user.getUserName() == users.get(i).getUserName() ||
+					user.getEmail() == users.get(i).getEmail())
 				return false;
 		}
 		return true;
 	}
-	public boolean signUp(User user)
-	{
-		if(searchUser(user))
-		{
+
+	public boolean signUp(User user) {
+		if (searchUser(user)) {
 			users.add(user);
 			System.out.println("Successfully signed up");
 			return true;
-		}
-		else
-			System.out.println("Try another E-mail or user name");	
+		} else
+			System.out.println("Try another E-mail or user name");
 		return false;
 	}
-	public boolean signIn(User user)
-	{
-		for(int i=0 ;i<users.size();i++)
-		{
-			if(user.getPassword()==users.get(i).getPassword()&&user.getEmail()==users.get(i).getEmail())
+
+	public boolean signIn(User user) {
+		for (int i = 0; i < users.size(); i++) {
+			if (user.getPassword() == users.get(i).getPassword()
+					&& user.getEmail() == users.get(i).getEmail())
 				return true;
 		}
 		return false;
