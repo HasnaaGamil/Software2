e4c8e25318e15e356960593097539d883b39c0da
HasnaaGamil <hasnaagamil333@gmail.com>
2022-12-05 17:55:19 +0200

    "Update serviceProvider"

--- src/ServiceProvider.java
+++ src/ServiceProvider.java
@@ -1,5 +1,29 @@
 public abstract class ServiceProvider {
 	Payment payment;
+	boolean acceptCash=false;
 	public abstract Service createService(String service);
-	
+	public void setPayment(Payment obj)
+	{
+		payment=obj;
+	}
+	public void performPayment()
+	{
+		
+		System.out.println("You can pay "+payment.pay()+" EGP");
+	}
+	public boolean getAcceptCash()
+	{
+		return acceptCash;
+	}
+	public void printPaymentMethods()
+	{
+		System.out.println("Choose the payment method: ");
+		System.out.println("1-Credit Card");
+		System.out.println("2-Wallet");
+		if(getAcceptCash()==true)
+		{
+			System.out.println("3-Cash");
+		}
+		
+	}
 }
