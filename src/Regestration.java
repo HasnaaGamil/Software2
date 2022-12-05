ceee2885e2f434b76aaff7db92d70f323bdb3abe
HasnaaGamil <hasnaagamil333@gmail.com>
2022-12-05 22:45:14 +0200

    Update Regestration.java

--- src/Regestration.java
+++ src/Regestration.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
 public class Regestration {
 
+	Scanner input = new Scanner(System.in);
 	List<User> users = new ArrayList<User>();
 
 	public boolean searchUser(User user) {
@@ -14,20 +15,37 @@ public class Regestration {
 		return true;
 	}
 
-	public boolean signUp(User user) {
-		if (searchUser(user)) {
+	public boolean signUp(User user)
+	{
+		System.out.println("Enter your email .");
+		String s=input.next();
+		user.setEmail(s);
+		System.out.println("Enter your user name .");
+		s=input.next();
+		user.setUserName(s);
+		System.out.println("Enter your password .");
+		s=input.next();
+		user.setPassword(s);
+		if(searchUser(user))
+		{
 			users.add(user);
 			System.out.println("Successfully signed up");
 			return true;
-		} else
-			System.out.println("Try another E-mail or user name");
+		}
+		else
+			System.out.println("Try another E-mail or user name");	
 		return false;
 	}
-
-	public boolean signIn(User user) {
-		for (int i = 0; i < users.size(); i++) {
-			if (user.getPassword() == users.get(i).getPassword()
-					&& user.getEmail() == users.get(i).getEmail())
+	public boolean signIn(User user)
+	{
+		System.out.println("Enter your email and password.");
+		String s=input.next();
+		user.setEmail(s);
+		s=input.next();
+		user.setPassword(s);
+		for(int i=0 ;i<users.size();i++)
+		{
+			if(user.getPassword()==users.get(i).getPassword()&&user.getEmail()==users.get(i).getEmail())
 				return true;
 		}
 		return false;
